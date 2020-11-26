57e6b7096648bc5b7aa01c7db7b29c8fcff61713
Maliik Morales <maliik@Maliiks-MacBook-Pro.local>
2020-11-25 19:07:49 -0500

    Utilized event delegation and targets to simplify delete and complete button code

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,40 @@
 const input = document.getElementById("input");
 const list = document.getElementById("list");
 const myDate = document.querySelector("#date");
-
-
-document.addEventListener('keyup', e => {
-  if (e.key === 'Enter' && input.value !== ''){
-    let item = input.value;
-    let position= 'beforeend';
-    let text = 
-    `
-    <li class='item'>
-      <i class="far fa-circle complete co" job="complete"></i>
-      <i class="far fa-check-circle incomplete co" job="complete"></i>
-      <p class="text"> ${item} </p>
-      <i class="fas fa-trash delete de" job="delete"></i>
-    </li>
-    `;
-    
-    list.insertAdjacentHTML(position, text);
-
-    input.value = '';
-
-    let trashIcons = document.querySelectorAll('.delete');
-    
-    trashIcons.forEach( e => {
-      e.addEventListener('click', () =>{
-        e.parentElement.remove();
-      });
-    });
-
-    let circleIcons = document.querySelectorAll('.complete');
-    let circleCheckIcons = document.querySelectorAll('.incomplete');
-
-    
-   
-    circleIcons.forEach( e => {
-      console.log();
-      e.addEventListener('click', () =>{
-        if(e.style.display = 'inline'){
-          e.style.display = 'none';
-          e.nextElementSibling.style.display = 'inline';
-          e.parentElement.childNodes[5].classList = "text lineThrough";
-        }
-        
-      });
-    });
-
-    circleCheckIcons.forEach( e => {
-      console.log();
-      e.addEventListener('click', () =>{
-        if(e.style.display = 'inline'){
-          e.style.display = 'none';
-          e.previousElementSibling.style.display = 'inline';
-          e.parentElement.childNodes[5].classList = "text";
-        }
-      });
-    });
-  };
-});
-
 let d = new Date();
 
 myDate.innerHTML = d.toLocaleDateString();
 
+input.addEventListener('keyup', e => {
+  if (e.key === 'Enter') {
+    let li = document.createElement('li');
+    let item = input.value;
+    li.className = 'item';
+    li.innerHTML = 
+      `
+      <i class="far fa-circle complete co"></i>
+      <i class="far fa-check-circle incomplete co"></i>
+      <p class="text"> ${item} </p>
+      <i class="fas fa-trash delete de"></i>
+      `;
+
+    input.value = '';
+    list.append(li);
+    
+  }
+});
+
+list.addEventListener('click', e => {
+  if(e.target.className === 'fas fa-trash delete de'){
+    e.target.parentElement.remove();
+  } else if(e.target.className === 'far fa-circle complete co'){
+    e.target.style.display = 'none';
+    e.target.nextElementSibling.style.display = 'block';
+    e.target.parentElement.childNodes[5].classList = "text lineThrough";
+  } else if(e.target.className === 'far fa-check-circle incomplete co'){
+    e.target.style.display = 'none';
+    e.target.previousElementSibling.style.display = 'block';
+    e.target.parentElement.childNodes[5].classList = "text";
+  }
+
+});
